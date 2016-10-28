ae4b63ca743da6a721aa36aae70bbcce5732419c
paul <paul>
2016-10-28 08:58:31 +0200

    init

--- presentmon.py
+++ presentmon.py
@@ -2,8 +2,6 @@
 # numpy and matplotlib using the common alias 'np' and 'plt'.
 import numpy as np
 import matplotlib.pyplot as plt
-import tkinter as tk
-from tkinter import filedialog
 
 plt.style.use('ggplot')
 
@@ -18,7 +16,7 @@ def plot(ax, times, values, bins, text):
 
     ax.scatter(times, values, color='g', alpha=0.3)
     ax.plot(x_new - delta / 2, running_median, 'b-', lw=2)
-    ax.legend(loc='lower right')
+    ax.legend()
     return
 
 
@@ -30,25 +28,19 @@ def plot_min_max_med(ax, values, text):
     ax.axhline(np.mean(values), label='average: ' + str(np.mean(values)),
                   alpha=0.4,
                   color='orange')
-    ax.legend(loc='lower right')
+    ax.legend()
     return
 
 
 # With matplotlib we define a new subplot with a certain size (10x10)
-fig, ax = plt.subplots(2, sharex=True)
+fig, ax = plt.subplots(2, sharex=True, figsize=(10, 10))
 
 # number of bins for median
-total_bins = 500
-
-# selct the presentmon csv-file
-root = tk.Tk()
-root.withdraw()
-file_path = filedialog.askopenfilename(initialdir='~', filetypes=(('Presentmon Files', '*.csv'), ('All Files', '*')))
+total_bins = 420
 
 # Using numpy we can use the function loadtxt to load your CSV file.
 # We ignore the first line with the column names and use ',' as a delimiter.
-# data = np.loadtxt('/home/paul/Schreibtisch/test.csv', delimiter=',', skiprows=1, usecols=(9, 10, 11, 12, 13, 14))
-data = np.loadtxt(file_path, delimiter=',', skiprows=1, usecols=(9, 10, 11, 12, 13, 14))
+data = np.loadtxt('/home/paul/Schreibtisch/test.csv', delimiter=',', skiprows=1, usecols=(9, 10, 11, 12, 13, 14))
 
 # You can access the columns directly, but let us just define them for clarity.
 # This uses array slicing/indexing to cut the correct columns into variables.
@@ -59,35 +51,25 @@ ms_in_present_api = data[:, 3]
 ms_until_render_complete = data[:, 4]
 ms_until_displayed = data[:, 5]
 
-# this are the important values: time and ms between presents (could calculate this, but hey it's already there)
-x = time_in_seconds
-y = ms_between_presents
+frames_per_sec = []
 
-fps = []
-last_y=0
-for tis in y:
-    if tis != -1:
-        fps.append(1 / (tis / 1000))
-        last_y = tis
-    else:
-        fps.append(1/ (last_y / 1000))
-        print(last_y)
+for tis in ms_between_presents:
+    frames_per_sec.append(1/(tis/1000))
 
 text="Frametime"
-plot(ax[0], x, y, total_bins, text)
-plot_min_max_med(ax[0], y, text)
+plot(ax[0], time_in_seconds, ms_between_presents, total_bins, text)
+plot_min_max_med(ax[0], ms_between_presents, text)
 ax[0].set_title(text)
 ax[0].set_ylim([0,20])
-ax[0].set_xlim([0,max(x)])
 
 text="FPS"
-plot(ax[1], x, np.asarray(fps), total_bins, text)
-plot_min_max_med(ax[1], fps, text)
+plot(ax[1], time_in_seconds, np.asarray(frames_per_sec), total_bins, text)
+plot_min_max_med(ax[1], frames_per_sec, text)
 ax[1].set_title(text)
-ax[1].set_ylim([0,300])
+ax[1].set_ylim([0,200])
 
 
 # Show the legend
 # plt.ylim([0, 200])
-plt.tight_layout()
+
 plt.show()
