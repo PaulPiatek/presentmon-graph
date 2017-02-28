ff457d676acb4c05e83cc1a87ecd4fd6d5d7f62c
paul <paul>
2017-02-28 22:40:27 +0100

    various improvements

--- presentmon.py
+++ presentmon.py
@@ -8,7 +8,8 @@ from tkinter import filedialog
 plt.style.use('ggplot')
 
 
-def plot(ax, times, values, bins, text):
+# renders the scatter and the median
+def plot_median_scatter(ax, times, values, bins):
     x_new = np.linspace(times.min(), times.max(), bins)
 
     delta = x_new[1] - x_new[0]
@@ -16,39 +17,46 @@ def plot(ax, times, values, bins, text):
 
     running_median = [np.median(values[idx == k]) for k in range(bins)]
 
-    ax.scatter(times, values, color='g', alpha=0.3)
-    ax.plot(x_new - delta / 2, running_median, 'b-', lw=2)
-    ax.legend(loc='lower right')
+    scatter = ax.scatter(times, values, color='g', alpha=0.3, label='raw')
+    median, = ax.plot(x_new - delta / 2, running_median, 'b-', lw=2, label='median')
+    return scatter, median
+
+
+# renders the average line and min/max text
+def plot_min_max_med(ax, values):
+    max_ms = "{0:.1f}".format(max(values))
+    min_ms = "{0:.1f}".format(min(values))
+    ax.text(0, 0, 'max/min ' + max_ms + '/' + min_ms, verticalalignment='bottom')
+
+    average = ax.axhline(np.mean(values), label='average: ' + str(np.mean(values)),
+                         color='orange',
+                         alpha=0.4)
+    return average
+
+
+# renders everything
+def plot(ax, times, values, bins):
+    scatter, median = plot_median_scatter(ax, times, values, bins)
+    average = plot_min_max_med(ax, values)
+    ax.legend(loc='lower right', handles=[scatter, median, average], scatterpoints=1)
     return
-
-
-def plot_min_max_med(ax, values, text):
-    max_ms = max(values)
-    min_ms = min(values)
-    ax.text(0, 1, 'max/min ' + text + ' ' + str(max_ms) + '/' + str(min_ms), verticalalignment='center')
-
-    ax.axhline(np.mean(values), label='average: ' + str(np.mean(values)),
-                  alpha=0.4,
-                  color='orange')
-    ax.legend(loc='lower right')
-    return
-
 
 # With matplotlib we define a new subplot with a certain size (10x10)
-fig, ax = plt.subplots(2, sharex=True)
+fig, axarr = plt.subplots(2, sharex=True, figsize=(14, 9), dpi=100)
 
 # number of bins for median
 total_bins = 500
 
-# selct the presentmon csv-file
+# select the presentmon csv-file
 root = tk.Tk()
 root.withdraw()
 file_path = filedialog.askopenfilename(initialdir='~', filetypes=(('Presentmon Files', '*.csv'), ('All Files', '*')))
 root.destroy()
+if not file_path:
+    exit(0)
 
 # Using numpy we can use the function loadtxt to load your CSV file.
 # We ignore the first line with the column names and use ',' as a delimiter.
-# data = np.loadtxt('/home/paul/Schreibtisch/test.csv', delimiter=',', skiprows=1, usecols=(9, 10, 11, 12, 13, 14))
 data = np.loadtxt(file_path, delimiter=',', skiprows=1, usecols=(9, 10, 11, 12, 13, 14))
 
 # You can access the columns directly, but let us just define them for clarity.
@@ -66,25 +74,22 @@ y = ms_between_presents
 
 # calculate fps from times
 fps = []
-last_y=0
+last_y = 0
 for tis in y:
     fps.append(1 / (tis / 1000))
 
-text="Frametime"
-plot(ax[0], x, y, total_bins, text)
-plot_min_max_med(ax[0], y, text)
-ax[0].set_title(text)
-ax[0].set_ylim([0,20])
-ax[0].set_xlim([0,max(x)])
+# render the frametime
+plot(axarr[0], x, y, total_bins)
+axarr[0].set_title("Frametime")
+axarr[0].set_ylim([0, 20])
+axarr[0].set_xlim([0, max(x)])
 
-text="FPS"
-plot(ax[1], x, np.asarray(fps), total_bins, text)
-plot_min_max_med(ax[1], fps, text)
-ax[1].set_title(text)
-ax[1].set_ylim([0,300])
+# render the fps
+plot(axarr[1], x, np.asarray(fps), total_bins)
+axarr[1].set_title("FPS")
+axarr[1].set_ylim([0, 300])
 
 
-# Show the legend
-# plt.ylim([0, 200])
+# show the plots
 plt.tight_layout()
 plt.show()
